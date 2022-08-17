297a3be5c9e78ce5bbc7987dd21c5a44ef65ad91
cwb22 <cwb22@cwb22ui-MacBookAir.local>
2022-08-17 23:23:57 +0900

    [feat] 이메일 로그인 기능 구현

--- lib/login.dart
+++ lib/login.dart
@@ -3,9 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'home.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  // firebase google login
   Future<UserCredential> signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -24,102 +30,140 @@ class Login extends StatelessWidget {
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
+  // firebase email login
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController(); //입력되는 값을 제어
+  final TextEditingController _passwordController = TextEditingController();
+  // email inputBox Widget
+  Widget _emailInputWidget() {
+    return TextFormField(
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'email'
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '이메일 입력하세요';
+        }
+        return null;
+      },
+    );
+  }
+  // password inputBox Widget
+  Widget _passwordInputWidget() {
+    return TextFormField(
+      controller: _passwordController,
+      obscureText: true,
+      keyboardType: TextInputType.visiblePassword,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'password'
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '비밀번호 입력하세요';
+        }
+        return null;
+      },
+    );
+  }
+
+  // fireabse login 시도
+  _login() async {
+    if(_formKey.currentState!.validate()) {
+      FocusScope.of(context).requestFocus(FocusNode());
+    }
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text, 
+        password: _passwordController.text
+      );
+    } on FirebaseAuthException catch (e) {
+      String message = '';
+
+      if (e.code == 'user-not-found') {
+        message = "사용자가 존재하지 않습니다.";
+      } else if (e.code == 'wrong-password') {
+        message = "비밀번호가 잘못 됐습니다.";
+      } else {
+        message = "이메일을 확인하세요.";
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Colors.red, 
+        )
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
               image: DecorationImage(
             image: AssetImage("assets/images/background.jpg"),
             fit: BoxFit.cover,
           )),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 100,
-                    width: 200,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Login",
-                      ),
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 300.0, left: 300.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 100),
+                  const Text(
+                    "Welcome to",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 60,
+                      color: Colors.black,
                     ),
                   ),
-                  SizedBox(
-                      height: 100,
-                      width: 200,
-                      child: TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "password",
-                        ),
-                      )),
                   Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        OutlinedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => Home()),
-                            );
-                          },
-                          child: Text("Log In"),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        ' AI ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 60,
+                          color: Colors.lightBlue,
                         ),
-                        SizedBox(
-                          width: 30,
-                        ),
-                        OutlinedButton(
-                          onPressed: () {},
-                          child: Text("Sign Up"),
-                        )
-                      ]),
-                  OutlinedButton(
-                    onPressed: signInWithGoogle,
-                    child: Text("Google Login"),
-                  ),
-                ],
-              ),
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text(
-                      "Welcome to",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                        color: Colors.black,
                       ),
-                    ),
-                    Row(
-                      children: [
-                        const Text(
-                          ' AI ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40,
-                            color: Colors.lightBlue,
-                          ),
+                      Text(
+                        "festival",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 60,
+                          color: Colors.black,
                         ),
-                        const Text(
-                          "festival",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40,
-                            color: Colors.black,
-                          ),
-                        )
-                      ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 70.0),
+                  _emailInputWidget(),
+                  const SizedBox(height: 10.0),
+                  _passwordInputWidget(),
+                  const SizedBox(height: 10.0),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: _login, 
+                      child: const Text('Login', style: TextStyle(fontSize: 22),)
                     )
-                  ])
-            ],
-          )),
+                  )
+                ]
+              ),
+            )
+          ) 
+        )
+      )
     );
   }
 }
